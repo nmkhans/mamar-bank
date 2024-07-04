44b4a3847304fd6b59da0ded355bd02ee201e366
nmkhans <nurmoin05@gmail.com>
2024-07-04 14:27:11 +0600

    login and logout added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('register/', views.UserRegisterView.as_view(), name = 'user-register')  
+  path('register/', views.UserRegisterView.as_view(), name = 'user-register'),
+  path('login/', views.UserLoginView.as_view(), name = 'user-login'),
+  path('logout/', views.user_logout, name = 'user-logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from .forms import UserRegisterForm
-from django.views.generic import FormView
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+from django.views.generic import FormView, View
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 class UserRegisterView(FormView):
@@ -13,3 +15,13 @@ class UserRegisterView(FormView):
     user = form.save()
     login(self.request, user)
     return super().form_valid(form)
+  
+class UserLoginView(LoginView):
+  template_name = 'accounts/user_login.html'
+  
+  def get_success_url(self):
+    return reverse_lazy('home')
+  
+def user_logout(req):
+  logout(req)
+  return redirect('home')
